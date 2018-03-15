7790f97e9dec09f0fbf0089453c5201864b814a2
hongbin wang <whongbin@mail.imu.edu.cn>
2018-03-15 09:37:23 +0800

    add encoder_num_blocks and decoder_num_blocks

--- hyperparams.py
+++ hyperparams.py
@@ -8,10 +8,10 @@ https://www.github.com/kyubyong/transformer
 class Hyperparams:
     '''Hyperparameters'''
     # data
-    source_train = 'corpora/train.tags.de-en.de'
-    target_train = 'corpora/train.tags.de-en.en'
-    source_test = 'corpora/IWSLT16.TED.tst2014.de-en.de.xml'
-    target_test = 'corpora/IWSLT16.TED.tst2014.de-en.en.xml'
+    source_train = 'corpora/train.clean.mo'
+    target_train = 'corpora/train.clean.ch'
+    source_test = 'corpora/test.clean.mo'
+    target_test = 'corpora/test.clean.ch'
     
     # training
     batch_size = 32 # alias = N
@@ -19,12 +19,15 @@ class Hyperparams:
     logdir = 'logdir' # log directory
     
     # model
-    maxlen = 10 # Maximum number of words in a sentence. alias = T.
+    maxlen = 50 # Maximum number of words in a sentence. alias = T.
                 # Feel free to increase this if you are ambitious.
-    min_cnt = 20 # words whose occurred less than min_cnt are encoded as <UNK>.
+    min_cnt = 0 # words whose occurred less than min_cnt are encoded as <UNK>.
+    eval_step = 1000
     hidden_units = 512 # alias = C
-    num_blocks = 6 # number of encoder/decoder blocks
-    num_epochs = 20
+    num_blocks = 3 # number of encoder/decoder blocks
+    encoder_num_blocks = 6 # number of encoder blocks, will cover num_blocks. 0 will unused this
+    decoder_num_blocks = 3 # number of decoder blocks, will cover num_blocks. 0 will unused this
+    num_epochs = 100
     num_heads = 8
     dropout_rate = 0.1
     sinusoid = False # If True, use sinusoid. If false, positional embedding.
