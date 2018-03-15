a21cd05947851ad21c19ff1ee47d034fbd16b265
hongbin wang <whongbin@mail.imu.edu.cn>
2018-03-15 09:38:50 +0800

    modify some codes

--- eval.py
+++ eval.py
@@ -27,7 +27,8 @@ def eval():
     X, Sources, Targets = load_test_data()
     de2idx, idx2de = load_de_vocab()
     en2idx, idx2en = load_en_vocab()
-     
+    print('sources.................')
+    print(Sources)
 #     X, Sources, Targets = X[:33], Sources[:33], Targets[:33]
      
     # Start session         
@@ -40,42 +41,44 @@ def eval():
               
             ## Get model name
             mname = open(hp.logdir + '/checkpoint', 'r').read().split('"')[1] # model name
-             
             ## Inference
             if not os.path.exists('results'): os.mkdir('results')
-            with codecs.open("results/" + mname, "w", "utf-8") as fout:
-                list_of_refs, hypotheses = [], []
-                for i in range(len(X) // hp.batch_size):
-                     
-                    ### Get mini-batches
-                    x = X[i*hp.batch_size: (i+1)*hp.batch_size]
-                    sources = Sources[i*hp.batch_size: (i+1)*hp.batch_size]
-                    targets = Targets[i*hp.batch_size: (i+1)*hp.batch_size]
-                     
-                    ### Autoregressive inference
-                    preds = np.zeros((hp.batch_size, hp.maxlen), np.int32)
-                    for j in range(hp.maxlen):
-                        _preds = sess.run(g.preds, {g.x: x, g.y: preds})
-                        preds[:, j] = _preds[:, j]
-                     
-                    ### Write to file
-                    for source, target, pred in zip(sources, targets, preds): # sentence-wise
-                        got = " ".join(idx2en[idx] for idx in pred).split("</S>")[0].strip()
-                        fout.write("- source: " + source +"\n")
-                        fout.write("- expected: " + target + "\n")
-                        fout.write("- got: " + got + "\n\n")
-                        fout.flush()
-                          
-                        # bleu score
-                        ref = target.split()
-                        hypothesis = got.split()
-                        if len(ref) > 3 and len(hypothesis) > 3:
-                            list_of_refs.append([ref])
-                            hypotheses.append(hypothesis)
-              
-                ## Calculate bleu score
-                score = corpus_bleu(list_of_refs, hypotheses)
-                fout.write("Bleu Score = " + str(100*score))
+            with codecs.open('results/'+mname+'.trans', 'w', 'utf8') as tfout:
+                with codecs.open("results/" + mname, "w", "utf-8") as fout:
+                    list_of_refs, hypotheses = [], []
+                    for i in range(len(X) // hp.batch_size):
+
+                        ### Get mini-batches
+                        x = X[i*hp.batch_size: (i+1)*hp.batch_size]
+                        sources = Sources[i*hp.batch_size: (i+1)*hp.batch_size]
+                        targets = Targets[i*hp.batch_size: (i+1)*hp.batch_size]
+
+                        ### Autoregressive inference
+                        preds = np.zeros((hp.batch_size, hp.maxlen), np.int32)
+                        for j in range(hp.maxlen):
+                            _preds = sess.run(g.preds, {g.x: x, g.y: preds})
+                            preds[:, j] = _preds[:, j]
+
+                        ### Write to file
+                        for source, target, pred in zip(sources, targets, preds): # sentence-wise
+                            got = " ".join(idx2en[idx] for idx in pred).split("</S>")[0].strip()
+                            fout.write("- source: " + source +"\n")
+                            fout.write("- expected: " + target + "\n")
+                            fout.write("- got: " + got + "\n\n")
+                            fout.flush()
+                            tfout.write(got)
+                            tfout.write('\n')
+
+                            # bleu score
+                            ref = target.split()
+                            hypothesis = got.split()
+                            if len(ref) > 3 and len(hypothesis) > 3:
+                                list_of_refs.append([ref])
+                                hypotheses.append(hypothesis)
+
+                    ## Calculate bleu score
+                    score = corpus_bleu(list_of_refs, hypotheses)
+                    fout.write("Bleu Score = " + str(100*score))
                                           
 if __name__ == '__main__':
     eval()
